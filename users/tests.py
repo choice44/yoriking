bc7fee6ab670dbd4bedfb16717d7fd79f53e70a2
Lee DoYoon <ehdbs1276@naver.com>
2023-05-28 01:46:06 +0900

    :white_check_mark::lock:Test add setUpTestData
    :white_ckeck_mark::lock:users/tests.py
    - 테스트 코드에서 setUpTestData를 추가하고 setUp을 수정하였습니다.

--- users/tests.py
+++ users/tests.py
@@ -6,15 +6,18 @@ from django.urls import reverse
 
 # 마이페이지 출력 테스트
 class MyPageTest(APITestCase):
-    def setUp(self):
+    @classmethod
+    def setUpTestData(self):
         self.user_data = {"email": "aaa@aaa.com", "password": "Test1847!"}
         self.user = User.objects.create_user("aaa@aaa.com", "Test1847!")
+
+    def setUp(self):
+        self.access_token = self.client.post(reverse('token_obtain_pair'), self.user_data).data['access']
         
     def test_get_user_data(self):
-        access_token = self.client.post(reverse('token_obtain_pair'), self.user_data).data['access']
         response = self.client.get(
             path=reverse("mypage"),
-            HTTP_AUTHORIZATION=f"Bearer {access_token}"
+            HTTP_AUTHORIZATION=f"Bearer {self.access_token}"
         )
         
         self.assertEqual(response.status_code, 200)
@@ -23,7 +26,8 @@ class MyPageTest(APITestCase):
 
 # 유저페이지 출력 테스트
 class UserPageTest(APITestCase):
-    def setUp(self):
+    @classmethod
+    def setUpTestData(self):
         # 유저 1 생성
         self.user_data = {"email": "aaa@aaa.com", "password": "Test1847!"}
         self.user = User.objects.create_user("aaa@aaa.com", "Test1847!")      
@@ -34,15 +38,17 @@ class UserPageTest(APITestCase):
         self.user2.set_password("password")        
         self.user2.save()
 
+    def setUp(self):
+        self.access_token = self.client.post(reverse('token_obtain_pair'), self.user_data).data['access']
+
 
     # 본인 페이지 출력 테스트 
-    def test_get_user_data(self):
-        access_token = self.client.post(reverse('token_obtain_pair'), self.user_data).data['access']
+    def test_get_user_data(self):        
         user_id = self.user.id
         url = reverse("user_view", kwargs={"user_id": user_id})
         response = self.client.get(
             path=url,
-            HTTP_AUTHORIZATION=f"Bearer {access_token}"
+            HTTP_AUTHORIZATION=f"Bearer {self.access_token}"
         )
 
         self.assertEqual(response.status_code, 200)
@@ -51,12 +57,11 @@ class UserPageTest(APITestCase):
 
     # 타인 페이지 출력 테스트
     def test_get_other_user_data(self):
-        access_token = self.client.post(reverse('token_obtain_pair'), self.user_data).data['access']
         user_id = self.user2.id
         url = reverse("user_view", kwargs={"user_id": user_id})
         response = self.client.get(
             path=url,
-            HTTP_AUTHORIZATION=f"Bearer {access_token}"
+            HTTP_AUTHORIZATION=f"Bearer {self.access_token}"
         )
 
         self.assertEqual(response.status_code, 200)
@@ -65,7 +70,8 @@ class UserPageTest(APITestCase):
 
 # 유저 페이지 수정 테스트
 class UserPageEditTest(APITestCase):
-    def setUp(self):
+    @classmethod
+    def setUpTestData(self):
         # 유저 1 생성
         self.user_data = {"email": "aaa@aaa.com", "password": "Test1847!"}
         self.user = User.objects.create_user("aaa@aaa.com", "Test1847!")
@@ -75,11 +81,12 @@ class UserPageEditTest(APITestCase):
         self.user2 = User.objects.create(email="bbb@bbb.com", password="Test1847!", nickname="bbb")
         self.user2.set_password("password")
         self.user2.save()
-
+        
+    def setUp(self):
+        self.access_token = self.client.post(reverse('token_obtain_pair'), self.user_data).data['access']
 
     # 본인 유저 페이지 수정
     def test_edit_user(self):
-        access_token = self.client.post(reverse('token_obtain_pair'), self.user_data).data['access']
         user_id = self.user.id
         url = reverse("user_view", kwargs={"user_id": user_id})
         response = self.client.put(
@@ -90,14 +97,13 @@ class UserPageEditTest(APITestCase):
                 "image":"edit_image_url_aaa"
             },
             format="json",
-            HTTP_AUTHORIZATION=f"Bearer {access_token}"
+            HTTP_AUTHORIZATION=f"Bearer {self.access_token}"
         )
 
         self.assertEqual(response.status_code, 200)
         
     # 타인 유저 페이지 수정
     def test_edit_other_user(self):
-        access_token = self.client.post(reverse('token_obtain_pair'), self.user_data).data['access']
         user_id = self.user2.id
         url = reverse("user_view", kwargs={"user_id": user_id})
         response = self.client.put(
@@ -108,7 +114,7 @@ class UserPageEditTest(APITestCase):
                 "image":"edit_image_url_bbb"
             },
             format="json",
-            HTTP_AUTHORIZATION=f"Bearer {access_token}"
+            HTTP_AUTHORIZATION=f"Bearer {self.access_token}"
         )
 
         self.assertEqual(response.status_code, 403)
@@ -116,12 +122,11 @@ class UserPageEditTest(APITestCase):
 
     # 타인 유저 탈퇴
     def test_delete_other_user(self):
-        access_token = self.client.post(reverse('token_obtain_pair'), self.user_data).data['access']
         user_id = self.user2.id
         url = reverse("user_view", kwargs={"user_id": user_id})
         response = self.client.delete(
             path=url,
-            HTTP_AUTHORIZATION=f"Bearer {access_token}"
+            HTTP_AUTHORIZATION=f"Bearer {self.access_token}"
         )
 
         self.assertEqual(response.status_code, 403)
@@ -129,19 +134,19 @@ class UserPageEditTest(APITestCase):
 
     # 본인 계정 탈퇴 
     def test_delete_user(self):
-        access_token = self.client.post(reverse('token_obtain_pair'), self.user_data).data['access']
         user_id = self.user.id
         url = reverse("user_view", kwargs={"user_id": user_id})
         response = self.client.delete(
             path=url,
-            HTTP_AUTHORIZATION=f"Bearer {access_token}"
+            HTTP_AUTHORIZATION=f"Bearer {self.access_token}"
         )
 
         self.assertEqual(response.status_code, 200)
 
 
 class UserFolloweTest(APITestCase):
-    def setUp(self):
+    @classmethod   
+    def setUpTestData(self):
         # 유저 1 생성
         self.user_data = {"email": "aaa@aaa.com", "password": "Test1847!"}
         self.user = User.objects.create_user("aaa@aaa.com", "Test1847!")
@@ -152,15 +157,16 @@ class UserFolloweTest(APITestCase):
         self.user2.set_password("password")        
         self.user2.save()
 
+    def setUp(self):
+        self.access_token = self.client.post(reverse('token_obtain_pair'), self.user_data).data['access']
 
     # 본인 팔로우 불가
-    def test_follow_self(self):
-        access_token = self.client.post(reverse('token_obtain_pair'), self.user_data).data['access']
+    def test_follow_self(self):        
         user_id = self.user.id
         url = reverse("follow_view", kwargs={"user_id": user_id})
         response = self.client.post(
             path=url,
-            HTTP_AUTHORIZATION=f"Bearer {access_token}"
+            HTTP_AUTHORIZATION=f"Bearer {self.access_token}"
         )
 
         self.assertEqual(response.status_code, 403)
@@ -169,12 +175,11 @@ class UserFolloweTest(APITestCase):
     # 타인 팔로우 확인
     def test_follow_self(self):
         # 팔로우 확인
-        access_token = self.client.post(reverse('token_obtain_pair'), self.user_data).data['access']
         user_id = self.user2.id
         url = reverse("follow_view", kwargs={"user_id": user_id})
         response = self.client.post(
             path=url,
-            HTTP_AUTHORIZATION=f"Bearer {access_token}"
+            HTTP_AUTHORIZATION=f"Bearer {self.access_token}"
         )
 
         self.assertEqual(response.status_code, 200)
